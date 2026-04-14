bad08f80b8455932861ba85bcb7f4130fe363526
Alexander <Alex.W.Schell@web.de>
2026-04-14 20:16:17 +0200

    Create Astro Starlight documentation

---
+++ docs-site/public/api/automata/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"foomp.automata"}];updateSearchResults();
---
+++ docs-site/public/api/automata/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html","k":"18"},{"m":"foomp.automata","l":"org.quurz.automata"},{"m":"foomp.automata","l":"org.quurz.automata.localisation"}];updateSearchResults();
