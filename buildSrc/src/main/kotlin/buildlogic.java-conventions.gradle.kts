1ef8cf2e83207521aef7639e1718223a4c3e47a1
Alexander <Alex.W.Schell@web.de>
2026-09-19 20:51:31 +0200

    Configure github-gradle-package(s)

--- buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
@@ -24,6 +24,14 @@ plugins {
 repositories {
     mavenCentral()
     mavenLocal()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/Quurz/Foomp")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 // Projektweite Metadaten
